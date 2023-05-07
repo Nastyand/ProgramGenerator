6009859007ab58b7fe2f67c934cafd2d32b7f256
Anastasia <aandreeva1313@mail.ru>
2023-05-07 15:54:34 +0700

    Генерируется код на c++

--- ProgramGenerator.pro
+++ ProgramGenerator.pro
@@ -3,4 +3,14 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    unit.cpp \
+    classunit.cpp \
+    methodunit.cpp \
+    printoperatorunit.cpp
+
+HEADERS += \
+    unit.h \
+    classunit.h \
+    methodunit.h \
+    printoperatorunit.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+#include "unit.h"
+#include "classunit.h"
+#include "methodunit.h"
+#include "printoperatorunit.h"
 
-using namespace std;
+std::string generateProgram() {
+    ClassUnit myClass( "MyClass" );
+    myClass.add(std::make_shared< MethodUnit >( "testFunc1", "void", 0 ),ClassUnit::PUBLIC);
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc2", "void", MethodUnit::STATIC ),
+        ClassUnit::PRIVATE
+    );
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST ),
+        ClassUnit::PUBLIC
+    );
+
+    auto method = std::make_shared< MethodUnit >( "testFunc4", "void", MethodUnit::STATIC );
+    method->add( std::make_shared< PrintOperatorUnit >( R"(Hello, world!\n)" ) );
+    myClass.add( method, ClassUnit::PROTECTED );
+
+    return myClass.compile();
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << generateProgram() << std::endl;
     return 0;
 }
